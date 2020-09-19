690de9ca34a65a2bb92b936796687a7eaab6bd9f
Jadson Luan <jadsonluan1@gmail.com>
2020-09-19 11:40:12 -0300

    Implementa record service

---
+++ backend/services/record.service.js
@@ -0,0 +1,15 @@
+const Record = require("../models/Record")
+
+module.exports = {
+  create: async({description, mood_id}) => {
+    if (!description) throw new Error("description not provided")
+    if (!mood_id) throw new Error("mood id not provided")
+
+    try {
+      return await Record.create({description, mood: mood_id})
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  },
+  findAll: async() => await Record.find({}).populate("mood")
+}
