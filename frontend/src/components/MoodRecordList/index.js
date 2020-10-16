290ffd5e39299fd4e9bbf4984a88acce60acb1cd
Jadson Luan <jadsonluan1@gmail.com>
2020-10-16 19:07:34 -0300

    Adiciona barra de filtro para MainPage

--- frontend/src/components/MoodRecordList/index.js
+++ frontend/src/components/MoodRecordList/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import MoodRecord from "./MoodRecord";
 import "./MoodRecordList.css";
 
-function MoodRecordList(props) {
+function MoodRecordList({moods, searchFor}) {
   return (
     <ul className="mood-record-list">
-      {props.moods.map(mood => <MoodRecord key={mood._id} mood={mood}/>)}
+      {moods
+        .filter(mood => mood.description.includes(searchFor) || mood.tag.name.includes(searchFor))
+        .map(mood => <MoodRecord key={mood._id} mood={mood}/>)}
     </ul>
   );
 }
--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainPage.css";
 import MoodRecordList from "../../components/MoodRecordList";
 
 function MainPage(props) {
+  const [search, setSearch] = useState("");
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === "search") {
+      setSearch(value);
+    }
+  }
+
   return (
     <div className="main-page">
       <h1>Seus últimos registros</h1>
-      <MoodRecordList moods={props.moods} />
+      <input type="text" name="search" value={search} onChange={handleChange} placeholder="Filtrar por... (tag ou descrição)"/>
+      <MoodRecordList moods={props.moods} searchFor={search} />
     </div>
   )
 }
