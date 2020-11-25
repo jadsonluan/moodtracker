38662223989c2c2fc0d536c96c8268aef4336c93
Jadson Luan <jadsonluan1@gmail.com>
2020-11-25 13:26:55 -0300

    Adiciona novas funções auxiliares

--- frontend/src/utils/mood-filter.js
+++ frontend/src/utils/mood-filter.js
@@ -2,6 +2,28 @@ export const months = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', '
 
 export const numberOfDays = (month, year) => (new Date(year, month + 1, 0)).getDate()
 
+const removeDuplicates = (tags) => {
+  const tagsWithoutDuplicates = {}
+  tags.forEach(tag => {
+    tagsWithoutDuplicates[tag._id] = tag
+  })
+  return Object.values(tagsWithoutDuplicates)
+}
+
+export function mostFrequentTag(moods) {
+  const frequency = {}
+  const tags = moods.map(mood => mood.tag)
+  tags.forEach(tag => frequency[tag.name] = frequency[tag.name] ? frequency[tag.name] + 1 : 1)
+  if (Object.values(frequency).length !== 0) {
+    const maxFrequency = Math.max(...Object.values(frequency))
+    const mostFrequents = Object.keys(frequency).filter(key => frequency[key] == maxFrequency)
+    const _tags = tags.filter(tag => mostFrequents.includes(tag.name))
+    const uniqueTags = removeDuplicates(_tags)
+    return uniqueTags
+  }
+  return []
+}
+
 export function findMoodsByDay(moods, date) {
   const result = moods.filter(mood => {
     let createdAt = mood.created_at instanceof Date ? mood.created_at : new Date(mood.created_at)
