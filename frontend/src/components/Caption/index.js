c291e69d580f09ecef8f60b00d4f322375a89b09
Jadson Luan <jadsonluan1@gmail.com>
2020-11-30 21:34:51 -0300

    Melhora design da página de visualizações

--- frontend/src/components/Caption/index.js
+++ frontend/src/components/Caption/index.js
@@ -7,7 +7,7 @@ function Caption(props) {
   const {tags} = useTags();
   return (
     <div className="caption">
-      <h3>Legenda</h3>
+      <b>Legenda</b>
       <div className="items">
         { tags.map(tag => <TagCaption key={tag._id} tag={tag}/>)}
       </div>
