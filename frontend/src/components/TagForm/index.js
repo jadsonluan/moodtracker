21540ecc71cc2ab355a73f697e4b7cb0faa17781
Jadson Luan <jadsonluan1@gmail.com>
2020-11-30 21:09:50 -0300

    Adiciona color picker para formulário de criação de tags

--- frontend/src/components/TagForm/index.js
+++ frontend/src/components/TagForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import Button from "../Button";
+import { TwitterPicker } from "react-color";
 import "./TagForm.css";
 
 function TagForm(props) {
@@ -24,21 +25,22 @@ function TagForm(props) {
     const { name, value } = ev.target
     if (name === "name") {
       setName(value);
-    } else if (name === "color") {
-      setColor(value); 
     }
   }
 
+  const handleColorChange = (color) => setColor(color.hex); 
+
   return (
     <div className="tagForm">
-      <h3>Nova Tag</h3>
+      <h3>Nova tag</h3>
 
       <div className="tagInput">
         <input type="text" name="name" value={name} className="rounded-input" onChange={handleChange} placeholder="Nome"></input>
       </div>
 
-      <div className="tagInput">
-        <input type="text" name="color" value={color} className="rounded-input" onChange={handleChange}></input> 
+      <div className="color-picker">
+        <b>Escolha uma cor</b>
+        <TwitterPicker color={color} onChangeComplete={handleColorChange}/>
       </div>
 
       <div className="buttons">
